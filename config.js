3459f5cddc88d56c51ad2005d56363707017bdb0
mcasalaspro <maurizio.casalaspro@gmail.com>
2026-09-12 20:38:14 -0300

    Atualizacao 12-09-2026 20:38

--- config.js
+++ config.js
@@ -5,4 +5,7 @@ window.CSP_CONFIG = {
   supabaseUrl: 'https://uytbslmuiauougzctrdk.supabase.co',
   supabaseAnonKey: 'sb_publishable_ylIXAw_d7-3d0TWQPXNDig_Hf8Zdzvk',
   homeUrl: '',
+  // background pictures from Unsplash (Settings → Picture source → Unsplash)
+  unsplashAccessKey: 'Yes3ObY5tIAqcdJYx2NgIK-6-XE7o1GomVcFt_HgmNw',
+  unsplashQuery: 'chess dark moody',
 };
